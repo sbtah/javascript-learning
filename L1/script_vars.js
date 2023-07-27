0cca9d420748956e791f95086663c2fa96eea79d
Sbtah <shisastore@protonmail.com>
2023-07-27 21:20:55 +0200

    P1 - Variables.

---
+++ L1/script_vars.js
@@ -0,0 +1,24 @@
+// Values and variables!
+
+console.log('Grzegorz');
+console.log(23);
+
+
+// Variable definition
+let firstName = 'Saruman';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let PI = 3.1415;
+
+
+let myFirtsJob = 'Gamer';
+let myCurrentJob = 'Programmer';
+
+
+// Assignments
+let country = 'Poland';
+let continent = 'Europe';
+let population = 'Who the fuck knows...';
