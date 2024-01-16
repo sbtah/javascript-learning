bb008e07402c5fa25b216ced02bdc6e9a35f8549
Sbtah <shisastore@protonmail.com>
2024-01-16 11:16:39 +0100

    Progress...

--- Jonas/if_else.js
+++ Jonas/if_else.js
@@ -1,1 +1,11 @@
 // Decission making..
+
+const age = 11;
+const isOldEnough = age >= 18;
+
+if(isOldEnough) {
+    console.log('This person can own a driving license.');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`This person can not own a driving license yet. Please wait ${yearsLeft} years.`)
+};
