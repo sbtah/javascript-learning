49e37ce9cffd2c8f41decbfb1bce93b506c27d2f
Sbtah <shisastore@protonmail.com>
2024-01-16 15:10:59 +0100

    Progress.

---
+++ Jonas/equality.js
@@ -0,0 +1,7 @@
+const age = 18;
+if (age === 18) {
+    console.log('You just became an adult!');
+};
+
+
+let favNumber = prompt('What is your favourite number?')
---
+++ Jonas/types.js
@@ -0,0 +1,17 @@
+// Types conversion.
+const inputYEar = '1991';
+
+
+console.log(inputYEar + 18);
+
+
+// Converting str to num
+console.log(Number(inputYEar) + 18);
+
+
+// Converting to str
+console.log(String(23));
+
+
+console.log('23' + '10' + 3);
+console.log('23' - '10' - 3);
