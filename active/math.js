14ff37cd67b5c37a06e4ac5880a06541724648ca
Sbtah <shisastore@protonmail.com>
2024-11-03 09:40:43 +0100

    Progress.

---
+++ active/math.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let x = 1;
+
+x = -x;
+console.log(x);
+
+
+let i = 1, y = 3;
+console.log(i - y);
+
+// String merging with +
+let s = 'my' + 'string';
+console.log(s);
+
+// Note, iff any of the operands is a string, then other one will be converted to a string too.
+console.log('1' + 20);
+console.log('2'+ 1000);
